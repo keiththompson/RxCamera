c3ed7768bd9936583c1d3f46c062b199c4026080
Keith Thompson <keithjoethompson@gmail.com>
2016-07-31 20:27:01 +0100

    stop using observable.create

--- rxcamera/src/main/java/com/ragnarok/rxcamera/RxCamera.java
+++ rxcamera/src/main/java/com/ragnarok/rxcamera/RxCamera.java
@@ -14,84 +14,80 @@ import com.ragnarok.rxcamera.config.RxCameraConfig;
 import com.ragnarok.rxcamera.request.RxCameraRequestBuilder;
 
 import rx.Observable;
-import rx.Subscriber;
+import rx.functions.Func0;
 import rx.functions.Func1;
 
 public class RxCamera  {
 
-    private RxCameraInternal cameraInternal = new RxCameraInternal();
+    private final RxCameraInternal cameraInternal;
 
-    private Matrix rotateMatrix = null;
+    private final Matrix rotateMatrix;
+
+    private RxCamera(Context context, RxCameraConfig config) {
+        this.cameraInternal = new RxCameraInternal(context, config);
+        this.rotateMatrix = new Matrix();
+        rotateMatrix.postRotate(getMatrixRotation(context), 0.5f, 0.5f);
+    }
 
     /**
-     * open the camera
+     * Open the camera
      * @param context
      * @param config
-     * @return
+     * @return An Observable<{@link RxCamera}>
      */
     public static Observable<RxCamera> open(final Context context, final RxCameraConfig config) {
-        return Observable.create(new Observable.OnSubscribe<RxCamera>() {
-            @Override
-            public void call(Subscriber<? super RxCamera> subscriber) {
+
+        return Observable.defer(new Func0<Observable<RxCamera>>() {
+            @Override public Observable<RxCamera> call() {
                 RxCamera rxCamera = new RxCamera(context, config);
                 if (rxCamera.cameraInternal.openCameraInternal()) {
-                    subscriber.onNext(rxCamera);
-                    subscriber.onCompleted();
-                } else {
-                    subscriber.onError(rxCamera.cameraInternal.openCameraException());
+                    return Observable.just(rxCamera);
                 }
+                return Observable.error(rxCamera.cameraInternal.openCameraException());
             }
         });
     }
 
     /**
-     * open camera and start preview, bind a {@link SurfaceView}
+     * Open the camera and start preview, bind a given {@link SurfaceView}
      * @param context
      * @param config
      * @param surfaceView
-     * @return
+     * @return An Observable<{@link RxCamera}>
      */
-    public static Observable<RxCamera> openAndStartPreview(Context context, RxCameraConfig config, final SurfaceView surfaceView) {
-        return open(context, config).flatMap(new Func1<RxCamera, Observable<RxCamera>>() {
-            @Override
-            public Observable<RxCamera> call(RxCamera rxCamera) {
-                return rxCamera.bindSurface(surfaceView);
-            }
-        }).flatMap(new Func1<RxCamera, Observable<RxCamera>>() {
-            @Override
-            public Observable<RxCamera> call(RxCamera rxCamera) {
-                return rxCamera.startPreview();
-            }
-        });
+    public static Observable<RxCamera> openAndStartPreview(Context context, RxCameraConfig config,
+                                                           final SurfaceView surfaceView) {
+        return open(context, config)
+            .flatMap(new Func1<RxCamera, Observable<RxCamera>>() {
+                @Override public Observable<RxCamera> call(RxCamera rxCamera) {
+                    return rxCamera.bindSurface(surfaceView);
+                }
+            }).flatMap(new Func1<RxCamera, Observable<RxCamera>>() {
+                @Override public Observable<RxCamera> call(RxCamera rxCamera) {
+                    return rxCamera.startPreview();
+                }
+            });
     }
 
     /**
-     * open camera and start preview, bind a {@link TextureView}
+     * Open the camera and start preview, bind a given {@link TextureView}
      * @param context
      * @param config
      * @param textureView
-     * @return
+     * @return An Observable<{@link RxCamera}>
      */
-    public static Observable<RxCamera> openAndStartPreview(Context context, RxCameraConfig config, final TextureView textureView) {
-        return open(context, config).flatMap(new Func1<RxCamera, Observable<RxCamera>>() {
-            @Override
-            public Observable<RxCamera> call(RxCamera rxCamera) {
-                return rxCamera.bindTexture(textureView);
-            }
-        }).flatMap(new Func1<RxCamera, Observable<RxCamera>>() {
-            @Override
-            public Observable<RxCamera> call(RxCamera rxCamera) {
-                return rxCamera.startPreview();
-            }
-        });
-    }
-
-    private RxCamera(Context context, RxCameraConfig config) {
-        this.cameraInternal = new RxCameraInternal();
-        this.cameraInternal.setConfig(config);
-        this.cameraInternal.setContext(context);
-        rotateMatrix = new Matrix();
-        rotateMatrix.postRotate(getMatrixRotation(context), 0.5f, 0.5f);
+    public static Observable<RxCamera> openAndStartPreview(final Context context, final RxCameraConfig config,
+                                                           final TextureView textureView) {
+        return open(context, config)
+            .flatMap(new Func1<RxCamera, Observable<RxCamera>>() {
+                @Override public Observable<RxCamera> call(RxCamera rxCamera) {
+                    return rxCamera.bindTexture(textureView);
+                }
+            }).flatMap(new Func1<RxCamera, Observable<RxCamera>>() {
+                @Override public Observable<RxCamera> call(RxCamera rxCamera) {
+                    return rxCamera.startPreview();
+                }
+            });
     }
 
     static int getMatrixRotation(Context context) {
@@ -113,110 +109,94 @@ public class RxCamera  {
     }
 
     /**
-     * bind a {@link SurfaceView} as the camera preview surface
+     * Bind a {@link SurfaceView} as the camera preview surface
      * @param surfaceView
-     * @return
+     * @return An Observable<{@link RxCamera}>
      */
     public Observable<RxCamera> bindSurface(final SurfaceView surfaceView) {
-        return Observable.create(new Observable.OnSubscribe<RxCamera>() {
-            @Override
-            public void call(Subscriber<? super RxCamera> subscriber) {
-                boolean result = cameraInternal.bindSurfaceInternal(surfaceView);
-                if (result) {
-                    subscriber.onNext(RxCamera.this);
-                    subscriber.onCompleted();
-                } else {
-                    subscriber.onError(cameraInternal.bindSurfaceFailedException());
+
+        return Observable.defer(new Func0<Observable<RxCamera>>() {
+            @Override public Observable<RxCamera> call() {
+                if (cameraInternal.bindSurfaceInternal(surfaceView)) {
+                    return Observable.just(RxCamera.this);
                 }
+                return Observable.error(cameraInternal.bindSurfaceFailedException());
             }
         });
     }
 
     /**
-     * bind a {@link TextureView} as the camera preview surface
+     * Bind a {@link TextureView} as the camera preview surface
      * @param textureView
-     * @return
+     * @return An Observable<{@link RxCamera}>
      */
     public Observable<RxCamera> bindTexture(final TextureView textureView) {
-        return Observable.create(new Observable.OnSubscribe<RxCamera>() {
-            @Override
-            public void call(Subscriber<? super RxCamera> subscriber) {
-                boolean result = cameraInternal.bindTextureInternal(textureView);
-                if (result) {
-                    subscriber.onNext(RxCamera.this);
-                    subscriber.onCompleted();
-                } else {
-                    subscriber.onError(cameraInternal.bindSurfaceFailedException());
+        return Observable.defer(new Func0<Observable<RxCamera>>() {
+            @Override public Observable<RxCamera> call() {
+                if (cameraInternal.bindTextureInternal(textureView)) {
+                    return Observable.just(RxCamera.this);
                 }
+                return Observable.error(cameraInternal.bindSurfaceFailedException());
             }
         });
     }
 
     /**
-     * start preview, must be called after bindTexture or bindSurface
-     * @return
+     * Start preview, must be called after bindTexture or bindSurface
+     * @return An Observable<{@link RxCamera}>
      */
     public Observable<RxCamera> startPreview() {
-        return Observable.create(new Observable.OnSubscribe<RxCamera>() {
-            @Override
-            public void call(Subscriber<? super RxCamera> subscriber) {
-                boolean result = cameraInternal.startPreviewInternal();
-                if (result) {
-                    subscriber.onNext(RxCamera.this);
-                    subscriber.onCompleted();
-                } else {
-                    subscriber.onError(cameraInternal.startPreviewFailedException());
+
+        return Observable.defer(new Func0<Observable<RxCamera>>() {
+            @Override public Observable<RxCamera> call() {
+                if (cameraInternal.startPreviewInternal()) {
+                    return Observable.just(RxCamera.this);
                 }
+                return Observable.error(cameraInternal.startPreviewFailedException());
             }
         });
     }
 
     /**
-     * close the camera, return an Observable as the result
-     * @return
+     * Close the camera, return an Observable as the result
+     * @return an Observable<Boolean>
      */
     public Observable<Boolean> closeCameraWithResult() {
-        return Observable.create(new Observable.OnSubscribe<Boolean>() {
-            @Override
-            public void call(Subscriber<? super Boolean> subscriber) {
-                subscriber.onNext(cameraInternal.closeCameraInternal());
-                subscriber.onCompleted();
+        return Observable.defer(new Func0<Observable<Boolean>>() {
+            @Override public Observable<Boolean> call() {
+                return Observable.just(cameraInternal.closeCameraInternal());
             }
         });
     }
 
     /**
-     * switch the camera, return an Observable indicated if switch success
-     * @return
+     * Switch the camera, return an Observable indicated if switch success
+     * @return an Observable<Boolean>
      */
     public Observable<Boolean> switchCamera() {
-        return Observable.create(new Observable.OnSubscribe<Boolean>() {
-            @Override
-            public void call(Subscriber<? super Boolean> subscriber) {
-                subscriber.onNext(cameraInternal.switchCameraInternal());
-                subscriber.onCompleted();
+        return Observable.defer(new Func0<Observable<Boolean>>() {
+            @Override public Observable<Boolean> call() {
+                return Observable.just(cameraInternal.switchCameraInternal());
             }
         });
     }
 
     /**
-     * return a {@link RxCameraRequestBuilder} which you can request the camera preview frame data
-     * @return
+     * @return a {@link RxCameraRequestBuilder} which you can request the camera preview frame data
      */
     public RxCameraRequestBuilder request() {
         return new RxCameraRequestBuilder(this);
     }
 
     /**
-     * return a {@link RxCameraActionBuilder} which you can change the camera parameter in the fly
-     * @return
+     * @return a {@link RxCameraActionBuilder} which you can change the camera parameter in the fly
      */
     public RxCameraActionBuilder action() {
         return new RxCameraActionBuilder(this);
     }
 
     /**
-     * directly close the camera
+     * Directly close the camera
      * @return true if close success
      */
     public boolean closeCamera() {
@@ -232,23 +212,23 @@ public class RxCamera  {
     }
 
     /**
-     * the config of this camera
-     * @return
+     * The config of this camera
+     * @return The {@link RxCameraConfig} for this camera
      */
     public RxCameraConfig getConfig() {
         return cameraInternal.getConfig();
     }
 
     /**
-     * the native {@link android.hardware.Camera} object
-     * @return
+     * @return The native {@link android.hardware.Camera} object
      */
+    @SuppressWarnings("deprecation")
     public Camera getNativeCamera() {
         return cameraInternal.getNativeCamera();
     }
 
     /**
-     * the final preview size, mostly this is not the same as the one set in {@link RxCameraConfig}
+     * The final preview size, mostly this is not the same as the one set in {@link RxCameraConfig}
      * @return
      */
     public Point getFinalPreviewSize() {
